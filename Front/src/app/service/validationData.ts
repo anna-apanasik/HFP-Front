cce1013c1c7cb5152bac32bc15751368ea5ce385
annablischik <annablischik@yandex.ru>
2018-03-02 23:38:30 +0300

    edit profile

--- Front/src/app/service/validationData.ts
+++ Front/src/app/service/validationData.ts
@@ -3,21 +3,10 @@ import {UserService} from "./userService";
 
 @Injectable()
 export class ValidationData {
-  errors = {
-    password: "INVALID_PASSWORD",
-    confirmPass: "PASSWORDS_NOT_EQUALS"
-  }
 
-  confirmPassword(password: string, confirm: string) {
-    if (password === confirm) {
-      return null;
-    } else {
-      return this.errors.confirmPass;
-    }
-  }
-
-  checkNick(nick: string) {
-    let regExp = new RegExp('[^A-Za-z]')
-    return regExp.test(nick);
+  checkConfirmPassword(password, confirmPassword) {
+    return password && confirmPassword
+      ? password === confirmPassword
+      : false;
   }
 }
